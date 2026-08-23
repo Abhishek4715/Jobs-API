451a380b942fb524f99e52b0965eff06335d1559
Abhishek <abhisheksahani4715@gmail.com>
2026-08-23 10:36:36 +0530

    Add helmet, cors, xss, rateLimit

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const app = express();
-
-// Authentication
 const AuthenticateUser = require("./middlewares/authentication.js");
-
-// MongoDB
 const connectDb = require("./db/connect.js");
-
-// Json
-app.use(express.json());
-
-// Route
 const jobs = require("./routes/jobsRoutes.js");
 const user = require("./routes/authsRoutes.js");
+const errorHandlerMiddleware = require("./middlewares/errorHandler.js");
+const notfound = require("./middlewares/notfound.js");
+const helmet =require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 
+app.use(express.json());
+
+// Authentication and Route
 app.use("/api/v1/jobs", AuthenticateUser, jobs);
 app.use("/api/v1/auths", user);
 
 // Error 
-const errorHandlerMiddleware = require("./middlewares/errorHandler.js");
-const notfound = require("./middlewares/notfound.js");
-
 app.use(notfound);
 app.use(errorHandlerMiddleware);
-// Home
 
+app.set("trust proxy", 1);
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000,
+        max: 100,
+    })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// Home
 app.get('/', (req, res) => {
     res.send("<h1>This is home page</h1>");
 })

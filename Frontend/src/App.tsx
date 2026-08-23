f871b8b23c36e6ae2fb47f046490ba3d769fcb76
Abhishek <abhisheksahani4715@gmail.com>
2026-08-23 17:25:20 +0530

    Start Files

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center'>
+      <h1 className='text-6xl m-4'>
+        API Home Page
+      </h1>
+    </div>
+  )
+}
+
+export default App
